54f73495ec94e310b21a5ad43ba3760b2f950bbe
Malak-Kanaan <malakkanaan17@gmail.com>
2019-12-16 15:21:27 +0200

    Completed Step_5.js

---
+++ Exercises/Javascript-Basics-01/Step_5.js
@@ -0,0 +1,6 @@
+function v() {
+  var num1 = document.getElementById("first_number").value;
+  var num2 = document.getElementById("second_number").value;
+  var mult = num1 * num2;
+  alert("The product is :" + mult);
+}
