8c380df8b122397ad58de8d44ead9b1ee8ce9166
Malak-Kanaan <malakkanaan17@gmail.com>
2019-12-16 15:35:20 +0200

    Completed Step_6.js

---
+++ Exercises/Javascript-Basics-01/Step_6.js
@@ -0,0 +1,7 @@
+function v() {
+  var num1 = document.getElementById("first_number").value;
+  var num2 = document.getElementById("second_number").value;
+
+  result = num1 % num2;
+  alert("The remainder of the division is :" + result);
+}
