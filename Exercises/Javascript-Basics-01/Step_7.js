6700553386bef0ceee278f541c98c37a22d13110
Malak-Kanaan <malakkanaan17@gmail.com>
2019-12-16 16:03:12 +0200

    Completed Step_7.js

---
+++ Exercises/Javascript-Basics-01/Step_7.js
@@ -0,0 +1,12 @@
+function v() {
+  var shoe = document.getElementById("shoe_size").value;
+  var yearrr = document.getElementById("year").value;
+  var result;
+  var result1 = shoe * 2;
+  var result2 = result1 + 5;
+  var result3 = result2 * 50;
+  var result4 = result3 - yearrr;
+  result = result4 + 1766;
+
+  alert("The result is :" + result);
+}
