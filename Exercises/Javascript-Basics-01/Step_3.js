44b6938fb1e2d7a417dcb086d6d2f1cd87a65361
Malak-Kanaan <malakkanaan17@gmail.com>
2019-12-16 14:46:42 +0200

    Completed Step_4.js

--- Exercises/Javascript-Basics-01/Step_3.js
+++ Exercises/Javascript-Basics-01/Step_3.js
@@ -1,3 +1,2 @@
 var name = prompt("What is your name?");
-alert("Hello !");
-alert(name);
+alert("Hello  " + name);
---
+++ Exercises/Javascript-Basics-01/Step_4.js
@@ -0,0 +1,6 @@
+var name = prompt("What is your name?");
+
+var surname = prompt("What is your surname?");
+
+var city = prompt("What is your city?");
+alert("Hello  " + name + "  " + surname + " from " + city);
