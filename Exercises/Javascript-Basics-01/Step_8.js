832cd3f745ba3bd12bd0e8bae1a5acc1183ab418
Malak-Kanaan <malakkanaan17@gmail.com>
2019-12-16 16:19:29 +0200

    Completed Step_8.js

---
+++ Exercises/Javascript-Basics-01/Step_8.js
@@ -0,0 +1,9 @@
+function v() {
+  var agee = document.getElementById("age").value;
+  if (agee < 18) {
+    alert("You are under 18");
+  }
+  if (agee >= 18) {
+    alert("You are over 18");
+  }
+}
